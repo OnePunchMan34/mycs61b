88dde00061e4ad08f42a759be4e23252830b08cd
OnePunchMan34 <tianq2@illinois.edu>
2020-05-12 21:39:46 -0500

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,16 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int s = 0;
+        //int x;
+        // x=0;
+        while (x < 10) {
+            s = s + x;
+            System.out.println(s + " ");
+            x = x + 1;
+        }
+    }
+}
+// static typing
+//Types are verified even berfore you run
+//Java is strongly typed
